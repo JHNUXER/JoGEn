8a53da89de6a407dc055955a4c974d08591ef71c
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 08:19:57 -0400

    Update Item.java

--- src/main/java/jogen/game/world/item/Item.java
+++ src/main/java/jogen/game/world/item/Item.java
@@ -1,1 +1,16 @@
-public class Item {}
+package jogen.game.world.item;
+
+import jogen.game.world.item.data.ItemID;
+
+public class Item {
+  public String name;
+  public ItemID id;
+  public String description;
+  
+  public boolean compare(Item b) {
+    return ItemID.compare(this.id, b.id);
+  }
+  public boolean compare(Item a, Item b) {
+    return ItemID.compare(a.id, b.id);
+  }
+}
