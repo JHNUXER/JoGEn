a1872f79af827e8037c3cfee690b7d4c30347bb1
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 08:16:56 -0400

    Update GameObject.java

--- src/main/java/jogen/game/world/object/GameObject.java
+++ src/main/java/jogen/game/world/object/GameObject.java
@@ -1,1 +1,18 @@
+package jogen.game.world.object;
 
+import jogen.game.world.object.data.position.*;
+import jogen.game.world.object.data.rotation.*;
+
+public class GameObject {
+  public Position position;
+  public Rotation rotation;
+  public RenderClass render;
+  public Composition madeOf;
+  
+  public float getVelocity() {
+    return JMath.avg({this.position.velocity.x, this.position.velocity.y, this.position.velocity.z});
+  }
+  public float getRotationSpeed() {
+    return JMath.avg({this.rotation.velocity.x, this.rotation.velocity.y, this.rotation.velocity.z});
+  }
+}
