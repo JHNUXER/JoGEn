34549ab058d714e0441036d3d495ca79d5fdc0b3
John Kayne <johnhoganuke2@gmail.com>
2016-08-12 08:24:13 -0400

    Update ItemID.java

--- src/main/java/jogen/game/world/item/data/ItemID.java
+++ src/main/java/jogen/game/world/item/data/ItemID.java
@@ -1,3 +1,13 @@
 package jogen.game.world.item.data;
 
-public class ItemID {}
+public class ItemID {
+  public String stringId;
+  public String uniqueId;
+  
+  public boolean compare(ItemID b) {
+    return (this.stringId == b.stringId && this.uniqueId == b.uniqueId);
+  }
+  public static boolean compare(ItemID a, ItemID b) {
+    return a.compare(b);
+  }
+}
