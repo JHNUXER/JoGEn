18dddaabef544ddcd0d79c233bfa3db35273f3c3
John Kayne <johnhoganuke2@gmail.com>
2016-09-20 08:21:44 -0400

    Update TLine.java

--- RG6/JoGEn_1/src/main/java/japi/filesystem/TLine.java
+++ RG6/JoGEn_1/src/main/java/japi/filesystem/TLine.java
@@ -1,3 +1,21 @@
 package japi.filesystem;
 
-import java.util.ArrayList;
+public class TLine {
+  public String line;
+  public String[] words;
+  public String[] characters;
+  
+  public TLine(String line) {
+    this.line = line;
+    this.words = line.split(" ");
+    this.characters = line.split("");
+  }
+  
+  public String[] wordsAfterFirst() {
+    String[] array = new String[this.words.length-1];
+    for (int i = 1; i < this.words.length; i++) {
+      array[i-1] = this.words[i];
+    }
+    return array;
+  }
+}
