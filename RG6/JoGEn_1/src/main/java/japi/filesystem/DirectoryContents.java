43922d33030e73a15cd41c73d9a7658a40ade495
John Kayne <johnhoganuke2@gmail.com>
2016-09-20 08:32:37 -0400

    Update DirectoryContents.java

--- RG6/JoGEn_1/src/main/java/japi/filesystem/DirectoryContents.java
+++ RG6/JoGEn_1/src/main/java/japi/filesystem/DirectoryContents.java
@@ -1,2 +1,54 @@
 package japi.filesystem;
 
+import java.io.File;
+
+public class DirectoryContents {
+  public ArrayList<File> directories = new ArrayList<File>();
+  public ArrayList<File> files = new ArrayList<File>();
+  
+  public DirectoryContents(JFile file) {
+    try {
+      if (!new File(file.path).isDirectory()) {
+        throw new Exeption("Folder (Directory) expected, Got File.");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    File[] fileList = file.listFiles();
+    
+    for (int i = 0; i < fileList.length; i++) {
+      if (fileList[i].isFile()) {
+        files.add(fileList[i]);
+      } else if (fileList[i].isDirectory()) {
+        directories.add(fileList[i]);
+      }
+    }
+  }
+  public DirectoryContents(String file) {
+    try {
+      if (!new File(file).isDirectory()) {
+        throw new Exeption("Folder (Directory) expected, Got File.");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    File[] fileList = new File(file).listFiles();
+    
+    for (int i = 0; i < fileList.length; i++) {
+      if (fileList[i].isFile()) {
+        files.add(fileList[i]);
+      } else if (fileList[i].isDirectory()) {
+        directories.add(fileList[i]);
+      }
+    }
+  }
+  
+  public int getSpaceConsumed() {
+    int j = 0;
+    for (int i = 0; i < files.length; i++) {
+      j += files.get(i).getTotalSpace();
+    }
+  }
+}
