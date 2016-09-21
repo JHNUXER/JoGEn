2f5963b3c2377c7b2506cab4bf37bcb4ab62402e
John Kayne <johnhoganuke2@gmail.com>
2016-09-21 09:06:41 -0400

    Update JMath.java

--- RG6/JoGEn_1/src/main/java/japi/math/JMath.java
+++ RG6/JoGEn_1/src/main/java/japi/math/JMath.java
@@ -3,6 +3,42 @@ package japi.math;
 import java.util.Random;
 
 public class JMath {
+  public static int average(int[] values) {
+    int sum = 0;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+    }
+    return sum / values.length;
+  }
+  public static float average(float[] values) {
+    float sum = 0.0f;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+    }
+  }
+  public static double average(double[] values) {
+    double sum = 0.0;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+    }
+  }
+  public static short average(short[] values) {
+    short sum = 0;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+    }
+  }
+  public static long average(long[] values) {
+    long sum = 0;
+    for (int i = 0; i < values.length; i++) {
+      sum += values[i];
+    }
+  }
+  
+  public static Object rand(Object[] values) {
+    return values[new Random().nextInt(values.length-1)];
+  }
+  
   public static String rand(String[] values) {
     return values[new Random().nextInt(values.length-1)];
   }
