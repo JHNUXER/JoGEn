c9864bf0f0e9b6b9503ebfe409f37bb75531c450
John Kayne <johnhoganuke2@gmail.com>
2016-09-21 09:22:32 -0400

    Update Display.java

--- RG6/JoGEn_1/src/main/java/jogen/core/Display.java
+++ RG6/JoGEn_1/src/main/java/jogen/core/Display.java
@@ -3,3 +3,28 @@ package jogen.core;
 import javax.swing.*;
 import javax.media.j3d.*;
 
+import japi.window.Window;
+
+public final class Display extends Window {
+  DisplayPanel panel;        // Panel where the game's Camera will render to.
+  JPanel backPanel;
+  
+  public static void init() {
+    panel = new DisplayPanel();
+    backPanel = new JPanel();
+    setContentPane(backPanel);
+    
+    setFullScreen(true);
+    setTitle("JoGEn");
+    setCloser();
+    show();
+  }
+  
+  public static void showGUI(JPanel gui) {
+    setContentPane(gui);
+  }
+  
+  public static void clear() {
+    setContentPane(backPanel);
+  }
+}
