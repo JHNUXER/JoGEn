b422a85cc3a699c6df9194802554165dba028dfa
John Kayne <johnhoganuke2@gmail.com>
2016-09-21 07:44:50 -0400

    Update JMath.java

--- RG6/JoGEn_1/src/main/java/japi/math/JMath.java
+++ RG6/JoGEn_1/src/main/java/japi/math/JMath.java
@@ -1,4 +1,9 @@
 package japi.math;
 
-import java.util.ArrayList;
+import java.util.Random;
 
+public class JMath {
+  public static String rand(String[] values) {
+    return values[new Random().nextInt(values.length-1)];
+  }
+}
