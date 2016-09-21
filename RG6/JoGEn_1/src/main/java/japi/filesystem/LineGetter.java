5699bc4ba66d6dddab184c5bc12889587953c542
John Kayne <johnhoganuke2@gmail.com>
2016-09-21 07:42:34 -0400

    Update LineGetter.java

--- RG6/JoGEn_1/src/main/java/japi/filesystem/LineGetter.java
+++ RG6/JoGEn_1/src/main/java/japi/filesystem/LineGetter.java
@@ -1,3 +1,13 @@
 package japi.filesystem;
 
-public final class LineGetter 
+import japi.filesystem.*;
+
+public final class LineGetter {
+  public static TLine[] get(String[] lines) {
+    TLine[] ret = new TLine[lines.length];
+    for (int i = 0; i < lines.length; i++) {
+      ret[i] = new TLine(lines[i]);
+    }
+    return ret;
+  }
+}
