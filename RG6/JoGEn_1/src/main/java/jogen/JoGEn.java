512dacef0c7ecca32a1943f8b7e23c8be58f0a85
John Kayne <johnhoganuke2@gmail.com>
2016-09-21 09:17:00 -0400

    Update JoGEn.java

--- RG6/JoGEn_1/src/main/java/jogen/JoGEn.java
+++ RG6/JoGEn_1/src/main/java/jogen/JoGEn.java
@@ -2,3 +2,9 @@ package jogen;
 
 import jogen.core.*;
 
+public final class JoGEn {
+  public static void main(String[] args) {
+    Display.init();
+    Display.showGUI(GUIs.GUI_MAIN_MENU);
+  }
+}
