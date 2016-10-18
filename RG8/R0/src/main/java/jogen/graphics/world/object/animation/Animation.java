ffac1c636549a7c1932ff8cf9ad8d4b0b65090dd
John Kayne <johnhoganuke2@gmail.com>
2016-10-18 10:49:52 -0400

    Update Animation.java

--- RG8/R0/src/main/java/jogen/graphics/world/object/animation/Animation.java
+++ RG8/R0/src/main/java/jogen/graphics/world/object/animation/Animation.java
@@ -1,3 +1,9 @@
 package jogen.graphics.world.object.animation;
 
-public class Animation
+public class Animation {
+  public Armature[] frames;
+  
+  public Animation(int frames) {
+    this.frames = new Armature[frames];
+  }
+}
