987382a66b0e8015cdce7a5e46ad7becc2c472fc
John Kayne <johnhoganuke2@gmail.com>
2016-10-18 10:46:33 -0400

    Update GameObject.java

--- RG8/R0/src/main/java/jogen/game/world/object/GameObject.java
+++ RG8/R0/src/main/java/jogen/game/world/object/GameObject.java
@@ -1,1 +1,13 @@
 package jogen.game.world.object;
+
+public class GameObject extends PhysicsObject {
+  public String name;
+  
+  public void tick(GameObject object) {
+    object.animation.tick();
+    object.armature.tick();
+    object.geometry.tick();
+    object.position.tick();
+    object.velocity.tick();
+  }
+}
