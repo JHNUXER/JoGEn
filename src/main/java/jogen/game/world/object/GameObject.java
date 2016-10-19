f3d312752a444a583c892e02539d521fb0a2ce27
John Kayne <johnhoganuke2@gmail.com>
2016-10-19 18:53:26 -0400

    Update GameObject.java

--- src/main/java/jogen/game/world/object/GameObject.java
+++ src/main/java/jogen/game/world/object/GameObject.java
@@ -1,3 +1,9 @@
 package jogen.game.world.object;
 
-public class GameObject
+public class GameObject extends PhysicsObject {
+  public String name;
+  
+  public GameObject() {
+    this.name = "Unnamed";
+  }
+}
