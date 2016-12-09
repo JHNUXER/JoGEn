8c4b6fcb1228059407ee9f86192e294e97bc7526
John Kayne <johnhoganuke2@gmail.com>
2016-12-09 08:28:26 -0500

    Update Inventory.java

--- RG8/R0/src/main/java/jogen/game/world/object/inventory/Inventory.java
+++ RG8/R0/src/main/java/jogen/game/world/object/inventory/Inventory.java
@@ -1,6 +1,21 @@
 package jogen.game.world.object.inventory;
 
-public class Inventory implements InventoryListener {
-  public void itemActuated(ItemEvent e) {
+public class Inventory {
+  public String name;
+  public InventorySlot[] slots;
+  
+  public Inventory() {
+    this("Unnamed Inventory",36);
+  }
+  public Inventory(int slots) {
+    this("Unnamed Inventory",slots);
+  }
+  public Inventory(InventorySlot[] slots) {
+    this("Unnamed Inventory",slots.length);
+    this.slots = slots;
+  }
+  public Inventory(String name,int slots) {
+    this.name = name;
+    this.slots = new InventorySlot[slots];
   }
 }
