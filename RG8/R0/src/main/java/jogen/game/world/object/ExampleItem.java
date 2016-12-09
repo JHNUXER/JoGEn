c9dc4d3ddc030af8f07ebb285dde7b011738946c
John Kayne <johnhoganuke2@gmail.com>
2016-12-09 07:58:06 -0500

    Update ExampleItem.java

--- RG8/R0/src/main/java/jogen/game/world/object/ExampleItem.java
+++ RG8/R0/src/main/java/jogen/game/world/object/ExampleItem.java
@@ -3,8 +3,12 @@ package jogen.game.world.object;
 public class ExampleItem extends Item {
   public String name = "Example Item";
   public String description = "Just an example of item code";
+  
   public void take(InventoryEvent e) {
     this.move(e.getActuator.inventory.emptySlot());
   }
-  public void give()
+  public void give() {
+    this.move(e.getActuated.inventory.emptySlot());
+  }
+  public void 
 }
