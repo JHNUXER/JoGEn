e38d2f6b0b4e1a065535bdffd302dd7b71516d01
John Kayne <johnhoganuke2@gmail.com>
2016-12-09 08:32:47 -0500

    Update World.java

--- RG8/R0/src/main/java/jogen/game/world/World.java
+++ RG8/R0/src/main/java/jogen/game/world/World.java
@@ -1,3 +1,19 @@
 package jogen.game.world;
 
-public class World {}
+public class World implements InventoryListener {
+  public String name;
+  public Player clientPOV;
+  
+  public ArrayList<Player> players = new ArrayList<Player>();
+  public ArrayList<Character> characters = new ArrayList<Character>();
+  public ArrayList<Item> items = new ArrayList<Item>();
+  public ArrayLIst<GameObject> objects = new ArrayList<GameObject>();
+  
+  public World(String name) {
+    this.name = name;
+  }
+  
+  public void inventoryActionPerformed(InventoryEvent e) {
+    
+  }
+}
