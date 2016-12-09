91a4439930bcb3f277bee477e32b4fa6f2c7e650
John Kayne <johnhoganuke2@gmail.com>
2016-12-09 10:09:30 -0500

    Update GraphicsObject.java

--- RG8/R1/src/main/java/jogen/graphics/world/object/GraphicsObject.java
+++ RG8/R1/src/main/java/jogen/graphics/world/object/GraphicsObject.java
@@ -3,13 +3,11 @@ package jogen.graphics.world.object;
 public abstract class GraphicsObject {
   public Position position;
   public Rotation rotation;
-  public Velocity velocity;
   public ArrayList<GeometryWithOffset> geometries = new ArrayList<GeometryWithOffset>();
   
   public static void tick(GraphicsObject object) {
     object.position.tick();
     object.rotation.tick();
-    object.velocity.tick();
   }
   public void tick() {
     tick(this);
