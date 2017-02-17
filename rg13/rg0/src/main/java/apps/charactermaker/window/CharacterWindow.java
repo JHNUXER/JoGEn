81300444fa90216eb622b46eefa027e1ae9540b5
John Kayne <johnhoganuke2@gmail.com>
2017-02-17 12:18:03 -0500

    Update CharacterWindow.java

--- rg13/rg0/src/main/java/apps/charactermaker/window/CharacterWindow.java
+++ rg13/rg0/src/main/java/apps/charactermaker/window/CharacterWindow.java
@@ -1,3 +1,24 @@
 public class CharacterWindow extends JFrame implements ActionListener {
+  public TabBox tabHolder;
+  public CharWindowMenuBar menubar;
   
+  public CharacterWindow() {
+    this.tabHolder = new TabBox();
+    this.menubar = new CharWindowMenuBar();
+    
+    add(tabHolder);
+    
+    setJMenuBar(menubar);
+    setTilte("New Character");
+    pack();
+    setResizable(true);
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+    setExtendedState(JFrame.MAXIMIZED_BOTH);
+  }
+  
+  public void actionPerformed(ActionEvent e) {
+    return;
+  }
 }
