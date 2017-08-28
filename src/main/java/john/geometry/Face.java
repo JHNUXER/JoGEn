032e85462c18f54c82ad3dfe1b992f93bff98724
John Kayne <johnhoganuke2@gmail.com>
2017-08-28 14:54:51 -0400

    Create Face.java

---
+++ src/main/java/john/geometry/Face.java
@@ -0,0 +1,15 @@
+package john.geometry;
+
+import java.util.*;
+
+public class Face {
+  public ArrayList<Point3D> vertices = new ArrayList<Point3D>();
+  public Material material;
+
+  public Face(Point3D[] verts,Material mat) {
+    for (int i = 0; i < verts.length; i++) {
+      this.vertices.add(verts[i]);
+    }
+    this.material = mat;
+  }
+}
