64fb14b38bc13ec3677f54e4c846bb8d2cf72874
John Kayne <johnhoganuke2@gmail.com>
2017-08-29 14:28:55 -0400

    Create KViewer.java

---
+++ src/main/java/john/graphics/KViewer.java
@@ -0,0 +1,10 @@
+package john.graphics;
+
+import java.awt.*;
+
+import john.data.Vector3;
+
+public class KViewer extends Dimension {
+  public Vector3<Double> position;
+  public Vector3<Double> rotation;
+}
