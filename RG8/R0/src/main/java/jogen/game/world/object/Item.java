0dfdbfe4ed6002934af2b8f547ed2f7107b34840
John Kayne <johnhoganuke2@gmail.com>
2017-08-29 11:25:19 -0400

    Delete Item.java

--- RG8/R0/src/main/java/jogen/game/world/object/Item.java
+++
@@ -1,9 +0,0 @@
-package jogen.game.world.object;
-
-import jogen.game.world.object.GameObject;
-import jogen.game.world.object.modifiers.Pocketable;
-
-public class Item extends GameObject implements Pocketable {
-  public String description;
-  public Character maker;
-}
