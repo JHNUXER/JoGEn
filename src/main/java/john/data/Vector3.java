6fcddfbb295b08933d8972f5d13616ba43b104af
John Kayne <johnhoganuke2@gmail.com>
2017-08-29 11:43:43 -0400

    Update Vector3.java

--- src/main/java/john/data/Vector3.java
+++ src/main/java/john/data/Vector3.java
@@ -1,7 +1,10 @@
 package john.data;
 
 public class Vector3<E extends Number> extends Point3<E extends Number> {
-  public Vector3 mul(Vector3 b) {  // dot prod
-    
+  public E dot(Vector3<E> b) {  // dot prod
+    return this.x*b.x+this.y*b.y+this.z*b.z;
+  }
+  public Vector3<E> cross(Vector3<E> b) {
+    return new Vector3<E>(this.y*b.z-this.z*b.y,this.z*b.x-this.x*b.z,this.x*b.y-this.y*b.x);
   }
 }
