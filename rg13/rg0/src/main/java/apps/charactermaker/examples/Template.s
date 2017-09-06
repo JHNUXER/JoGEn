a0f2308bc6335ba9150157d10a0472b1511ab97d
John Kayne <johnhoganuke2@gmail.com>
2017-09-06 11:02:04 -0400

    Create Template.s

---
+++ rg13/rg0/src/main/java/apps/charactermaker/examples/Template.s
@@ -0,0 +1,16 @@
+Character {
+  name = "Dale",
+  species = $Human,
+  gender = $Male,
+  body = {
+    height = 6ft 1in,
+    hair = null,
+    eyes = {
+      both = {
+        eye = #ffffff,
+        iris = #608040,
+        pupil = #000000
+      }
+    }
+  };
+}
