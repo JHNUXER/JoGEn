90a91402754c0f87927d9f22219e9f897789dfc6
John Kayne <johnhoganuke2@gmail.com>
2017-10-25 14:42:58 -0400

    Update Matrix.java

--- src/main/java/john/math/Matrix.java
+++ src/main/java/john/math/Matrix.java
@@ -8,4 +8,16 @@ public class Matrix<E extends Number> {
   public E[][] value() {
     return m;
   }
+  
+  public Matrix<E> add(Matrix<E> b) {
+    for (int x = 0; x < m.length; x++) {
+      for (int y = 0; y < m[x].length; y++) {
+        for (int h = 0; h < b.m.length; h++) {
+          for (int k = 0; k < b.m[h].length; k++) {
+            this.m[x][y] += b.m[h][k];
+          }
+        }
+      }
+    }
+  }
 }
