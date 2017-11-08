0baeffa296136599ae305f18a177ed5b9ed36d2e
John Kayne <johnhoganuke2@gmail.com>
2017-11-08 14:21:08 -0500

    Create Particle.java

---
+++ src/main/java/john/graphics/Particle.java
@@ -0,0 +1,10 @@
+package john.graphics;
+
+import java.awt.image.BufferedImage;
+import john.math.Point3D;
+
+public class Particle {
+  public BufferedImage texture;
+  public Point3D position;
+  public double[] lifetime;
+}
